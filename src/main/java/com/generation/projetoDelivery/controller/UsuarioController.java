9758e74bc01eac9c2e5e51f79883de58f0dcefde
Mina Yura <minammonteiro2@gmail.com>
2025-03-13 16:31:31 -0300

    att usuariocontroller

--- src/main/java/com/generation/projetoDelivery/controller/UsuarioController.java
+++ src/main/java/com/generation/projetoDelivery/controller/UsuarioController.java
@@ -13,8 +13,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.generation.projetoDelivery.model.Categoria;
 import com.generation.projetoDelivery.model.Usuario;
 import com.generation.projetoDelivery.repository.UsuarioRepository;
 
