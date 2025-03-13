8383f446255ea98719cfeb1fc7ab798c7ab880df
Beatriz Corrêa <bcorreacontato@gmail.com>
2025-03-13 11:04:35 -0300

    add model categoria

--- src/main/java/com/generation/projetoDelivery/repository/CategoriaRepository.java
+++ src/main/java/com/generation/projetoDelivery/repository/CategoriaRepository.java
@@ -1,5 +1,17 @@
 package com.generation.projetoDelivery.repository;
 
-public interface CategoriaRepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+
+import com.generation.projetoDelivery.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
+	
+	
+	public List<Categoria> findAllByDescricaoContainingIgnoreCase(@Param("nome") String nome);
+	
 
 }
