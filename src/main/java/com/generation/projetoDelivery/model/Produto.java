044c0cfe293e124d76217ffb3d5e508d19315ab2
LA1SA0 <ferreirlaisa@gmail.com>
2025-03-13 11:04:23 -0300

    Adicionando Model Produto

--- src/main/java/com/generation/projetoDelivery/model/Produto.java
+++ src/main/java/com/generation/projetoDelivery/model/Produto.java
@@ -1,5 +1,91 @@
 package com.generation.projetoDelivery.model;
 
+import org.springframework.data.annotation.Id;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+
+
+@Entity
+@Table(name = "tb_produtos")
 public class Produto {
 
+	    @Id
+	    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	    private Long id;
+	    
+	    @NotBlank (message = "O atributo nome é obrigatório!")
+	    private String nome;
+	    
+	    @NotBlank (message = "O atributo Descrição é obrigatório!")
+	    private String descricao;
+	    
+	    @NotNull (message = "O atributo preço é obrigatório!")
+	    private BigDecimal preco;
+	    
+	    @NotNull (message = "O atributo estoque é obrigatório!")
+	    private Double estoque;
+	    
+	    @OneToMany
+	    @JoinColumn(name = "tb_categoria_id")
+	    private Categoria categoria;
+
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public String getNome() {
+			return nome;
+		}
+
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+
+		public String getDescricao() {
+			return descricao;
+		}
+
+		public void setDescricao(String descricao) {
+			this.descricao = descricao;
+		}
+
+		public BigDecimal getPreco() {
+			return preco;
+		}
+
+		public void setPreco(BigDecimal preco) {
+			this.preco = preco;
+		}
+
+		public Double getEstoque() {
+			return estoque;
+		}
+
+		public void setEstoque(Double estoque) {
+			this.estoque = estoque;
+		}
+
+		public Categoria getCategoria() {
+			return categoria;
+		}
+
+		public void setCategoria(Categoria categoria) {
+			this.categoria = categoria;
+		}
+	    
+	    
+	
 }
