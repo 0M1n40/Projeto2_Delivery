ea9282927ab11481d9431992147b6db338265b9b
Laura Carneiro de Azevedo <azevedolaura02@gmail.com>
2025-03-13 11:42:58 -0300

    feat: implement UsuarioRepository

--- src/main/java/com/generation/projetoDelivery/repository/UsuarioRepository.java
+++ src/main/java/com/generation/projetoDelivery/repository/UsuarioRepository.java
@@ -1,5 +1,12 @@
 package com.generation.projetoDelivery.repository;
 
-public interface UsuarioRepository {
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.generation.projetoDelivery.model.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+	
+	public Optional<Usuario> findByUsuario(String usuario);
 }
