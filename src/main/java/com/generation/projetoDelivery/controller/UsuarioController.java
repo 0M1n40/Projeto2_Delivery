21e51cd1cbd61e0bda02487b53df99c12015e91e
Mina Yura <minammonteiro2@gmail.com>
2025-03-13 15:42:55 -0300

    Adicionado a UsuarioController

--- src/main/java/com/generation/projetoDelivery/controller/UsuarioController.java
+++ src/main/java/com/generation/projetoDelivery/controller/UsuarioController.java
@@ -1,5 +1,67 @@
 package com.generation.projetoDelivery.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.generation.projetoDelivery.model.Usuario;
+import com.generation.projetoDelivery.repository.UsuarioRepository;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/usuarios")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class UsuarioController {
+
+	
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+
+	@GetMapping("/all")
+	public ResponseEntity<List<Usuario>> getAll() {
+
+		return ResponseEntity.ok(usuarioRepository.findAll());
+
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Usuario> getById(@PathVariable Long id) {
+		return usuarioRepository.findById(id)
+				.map(resposta -> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	
+	@PostMapping
+	public ResponseEntity<Usuario> post(@RequestBody @Valid Usuario usuario) {
+		if (usuarioRepository.findById(usuario.getId()).isPresent()) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+		}
+
+		Usuario novoUsuario = usuarioRepository.save(usuario);
+		return ResponseEntity.status(HttpStatus.CREATED).body(novoUsuario);
+	}
+
+	@PutMapping
+	public ResponseEntity<Usuario> putUsuario(@Valid @RequestBody Usuario usuario) {
+		if (usuarioRepository.existsById(usuario.getId())) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
+
+		Usuario usuarioAtualizado = usuarioRepository.save(usuario);
+		return ResponseEntity.status(HttpStatus.OK).body(usuarioAtualizado);
+	}
 
 }
